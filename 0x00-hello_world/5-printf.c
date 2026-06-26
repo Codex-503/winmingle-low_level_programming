282f6089a848064781956a2989abe21583272a36
Codex-503 <godspraise977@gmail.com>
2026-06-26 12:35:29 +0100

    Add program that prints message with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Author: OGIDI-SUNDAY GODSPRAISE
+* Program: WinMingle C Training Program 
+* Description: outputting using prints
+*/
+int main(void) {
+    printf("with proper grammar, but the outcome is a piece of art\n");
+    return(0);
+}
