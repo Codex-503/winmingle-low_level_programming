82a041807ad42ae13c73841806237415f9182ce7
Codex-503 <godspraise977@gmail.com>
2026-06-26 12:32:15 +0100

    Add program that prints message using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Author: OGIDI-SUNDAY GODSPRAISE
+*PROGRAM: WinMingle Community C Training
+*Description: C script that outputs using puts
+*/
+
+int main(void) {
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
